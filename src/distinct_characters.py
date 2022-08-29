04f91778f195750f636bbd32de29f1651703c7a9
modengann <99417989+modengann@users.noreply.github.com>
2022-08-29 11:48:48 -0400

    Update distinct_characters.py

--- src/distinct_characters.py
+++ src/distinct_characters.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 
 def distinct_characters(L):
-    result = {}
-    for x in L:
-        result[x] = len(set(x))
-
-    return result
+    pass
 
 def main():
     print(distinct_characters(["check", "look", "try", "pop"]))
